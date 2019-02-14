2933f4187f7ff767a9f6cf53af0952367346a109
brianmle86 <brianmle862@gmail.com>
2019-02-14 15:01:28 -0800

    ....

---
+++ question.cpp
@@ -0,0 +1,22 @@
+/**
+ Lab 3:
+ 
+Author: Brian Le
+Date:
+    Due: 02/8/19
+Completed: 02/15/19
+*/
+
+#include<iostream>
+#include "Question.h"
+using namespace std;
+
+int main() {
+    Question questions[10];
+    questions[0].setQuestion("What city is De Anza located?");
+    questions[0].setAnswer1("Cupertino");
+    questions[0].setAnswer2("San Jose");
+    questions[0].setAnswer3("Santa Clara");
+    questions[0].setAnswer4("Sunnyvale");
+
+}
