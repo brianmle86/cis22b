dc31aba0b2eb4e9ce36f2a48a799d1f5a4ac6a5a
brianmle86 <brianmle862@gmail.com>
2019-03-07 13:57:18 -0800

    debug

--- Encrypted.cpp
+++ Encrypted.cpp
@@ -1,12 +1,47 @@
-#include <Encrypted.h>
+#include "Encrypted.h"
 #include <fstream>
-
-Encrypted(string filename, int max) {
+#include <string>
+#include <iostream>
+using namespace std;
+Encrypted::Encrypted(string filename, int max) {
+    status = true;
     ifstream inFile;
     inFile.open(filename);
     if(!inFile) {
         cout << "could not open file\n";
         exit(1);
     }
-    char *message = new char[max];
+    message = new char[max + 1];
+    char x;
+    char first_ch;
+    while(inFile >> x) {
+        status = false;
+        int i;
+        message[i] = x;
+        i++;
+    }
+}
+
+bool Encrypted::isEmpty() {
+    return status;
+}
+
+void Encrypted::decode() {
+    char alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
+    char ch;
+    //compare key to message
+        //find index of alphabet in file;
+    while(inFile >> ch) {
+        
+    }
+        //compare to key
+        //swap values
+        //store in message char array
+}
+
+void Encrypted::print() {
+
+}
+Encrypted::~Encrypted() {
+    delete[] message;
 }
--- Encrypted.h
+++ Encrypted.h
@@ -1,13 +1,15 @@
+#include <string>
+using namespace std;
 #ifndef Encrypted_h
 #define Encrypted_h
 class Encrypted {
     private:
         char *message;
-        const char key[] = "iztohndbeqrkglmacsvwfuypjx";
+        const char key[27] = "iztohndbeqrkglmacsvwfuypjx";
         bool status;
     public:
         Encrypted();
-        Encrypted(string filename, int max)
+        Encrypted(std::string filename, int max);
         bool isEmpty();
         void decode();
         void print();
