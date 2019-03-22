ae4eb694d50d9afc2f7019598627206eb5e98b47
brianmle86 <brianmle862@gmail.com>
2019-03-22 13:56:23 -0700

    lab 6 works

--- LoShuSquare.h
+++ LoShuSquare.h
@@ -1,43 +1,102 @@
 
 #ifndef LoShuSquare_h
 #define LoShuSquare_h
-
+#include <iostream>
 using namespace std;
 
 class LoShuSquare {
     private:
-        int number;
+        int magicSum = 15; //sum must be 15, so we call this magic sum
         int grid[3][3];
     public:
         LoShuSquare();
-        ~LoShuSqaure();
+        ~LoShuSquare();
         void fill(int row, int col, int num);
-        bool checkRow();
-        bool checkCol();
-        bool checkDiag();
+        bool checkRow(int array[][3]);
+        bool checkCol(int array[][3]);
+        bool checkDiag(int array[][3]);
+        bool isLoShuSquare();
         void printGrid();
 };
 
 LoShuSquare::LoShuSquare() {
-    grid = { {0, 0, 0},
-             {0, 0, 0},
-             {0, 0, 0} };
-              
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++)
+            grid[i][j] = 0;
+    }
 }
 
-LoShuSquare::void fill(int row, int col, int num) {
-    grid[row - 1][col - 1] = num;
+LoShuSquare::~LoShuSquare() {};
+
+void LoShuSquare::fill(int row, int col, int num) {
+    grid[row][col] = num;
 }
 
-LoShuSquare:: void printGrid(int array[][]) {
+bool LoShuSquare::checkRow(int array[][3]) {
+    int sum = 0;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++)
+            sum += array[i][j];
+        
+        if(sum != magicSum)
+            return false; //if one row is not 15, returns false
+        else
+            sum = 0;
+    } 
+    return true; //all rows sum to 15, so return true
+}
+
+bool LoShuSquare::checkCol(int array[][3]) {
+    int sum = 0;
+    for(int j = 0; j < 3; j++) {
+        for(int i = 0; i < 3; i++)
+            sum += array[i][j];
+        
+        if(sum != magicSum)
+            return false;
+        else
+            sum = 0;
+    }
+    return true;
+}
+
+bool LoShuSquare::checkDiag(int array[][3]) {
+    int sum = 0;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            if(i == j)
+                sum += array[i][j];
+        }
+    }
+    
+    if(sum != magicSum)
+        return false;
+    else
+        sum = 0;
+    
+    sum = array[0][2] + array[1][1] + array[2][0];
+    if(sum != magicSum)
+        return false;
+    else
+        return true;
+}
+
+bool LoShuSquare::isLoShuSquare() {
+    if(checkRow(grid) && checkCol(grid) && checkDiag(grid))
+        return true;
+    else
+        return false;
+}
+
+void LoShuSquare::printGrid() {
     cout << "==================================";
     for(int i = 0; i < 3; i++) {
         cout << endl;
-        for(int j = 0; i < 3; i++) {
-            cout << array[i][j] << " ";
+        for(int j = 0; j < 3; j++) {
+            cout << grid[i][j] << " ";
         }
     }
-    cout << endl
-         << "==================================";
+    cout << endl << "==================================\n";
 }
+
 #endif
--- Player.h
+++ Player.h
@@ -1,14 +1,70 @@
+
 #include "LoShuSquare.h"
+#include <string>
+#include <iostream>
+
 #ifndef Player_h
 #define Player_h
 
 using namespace std;
 
 class Player {
-    private:
-
+    protected:
+        LoShuSquare game;
+        string name;
     public:
         Player(LoShuSquare game);
         ~Player();
+};
+
+Player::Player(LoShuSquare game) {
+    int row, column, value;
+    char input;
+    cout << "A Lo Shu Square is a 3x3 grid which\n"
+         << "consists of numbers 1 - 9. All rows, columns,\n"
+         << "and diagonals must add up to 15.\n\n";
+
+    cout << "Here is an empty grid.\n";
+    game.printGrid();
+
+    cout << "Please fill out the values of the grid:\n";
+    int number;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cout << "Enter value of row " << (i + 1)
+                 << ", column " << (j + 1) << ": ";
+            cin >> number;
+            game.fill(i, j, number);
+            cout << endl;
+        }
+    }
+    
+    game.printGrid();
+    
+    cout << "Would you like to change a specific value\n"
+         << "in the grid? (y/n): ";
+    cin >> input;
+    while(input == 'Y' || input == 'y') {
+        cout << "Enter row, column, and value: ";
+        cin >> row >> column >> value;
+        row -= 1;
+        column -= 1;
+        game.fill(row, column, value);
+        
+        game.printGrid();
+        
+        cout << "Change another value? (y/n): ";
+        cin >> input;
+    }
+
+    bool isLoShuSquare = game.isLoShuSquare();
+    if(isLoShuSquare) {
+        cout << "The grid is a Lo Shu Square.\n";
+    }
+    else {
+        cout << "The grid is not a Lo Shu Square.\n";
+    }
+
 }
+Player::~Player() {};
 #endif
